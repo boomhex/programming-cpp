42ce6d9647e1a6b08dfbaa4eb67f8b8fb3a435c2
boomhex <98780985+boomhex@users.noreply.github.com>
2025-11-27 10:02:35 +0100

    fix indentation 15

--- set2/15/example2.cc
+++ set2/15/example2.cc
@@ -2,38 +2,38 @@
 
 class obj
 {
-  public:
-    obj();
-    ~obj();
+    public:
+        obj();
+        ~obj();
 };
 
 obj::obj()
 {
-  std::cout << "obj constructor\n";
+    std::cout << "obj constructor\n";
 }
 
 obj::~obj()
 {
-  std::cout << "obj destructor\n";
+    std::cout << "obj destructor\n";
 }
 
 void func()
 {
-  obj object;   // local object (RAII)
-  std::cout << "func: something went wrong, throwing exception...\n";
-  throw std::string{"func failed"};   // stack unwinding, calling the objects destructor
+    obj object;   // local object (RAII)
+    std::cout << "func: something went wrong, throwing exception...\n";
+    throw std::string{"func failed"};   // stack unwinding, calling the objects destructor
 }
 
 int main()
 {
-  try
-  {
-    func();
-  }
-  catch (std::string str )
-  {
-    std::cout << "main: caught exception: " << str << '\n';
-  }
+    try
+    {
+        func();
+    }
+    catch (std::string str )
+    {
+        std::cout << "main: caught exception: " << str << '\n';
+    }
 
-  std::cout << "main: end\n";  // the program still executes
+    std::cout << "main: end\n";  // the program still executes
 }
