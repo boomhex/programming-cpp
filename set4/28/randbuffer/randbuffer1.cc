2a1fcb986acfafee2eb7c777256790d0487a2996
boomhex <98780985+boomhex@users.noreply.github.com>
2025-12-11 22:59:59 +0100

    indent

--- set4/28/randbuffer/randbuffer1.cc
+++ set4/28/randbuffer/randbuffer1.cc
@@ -1,11 +1,12 @@
 #include "randbuffer.ih"
 
 RandBuffer::RandBuffer(int min, int max, size_t seed)
-    : d_min(min),
-      d_range(max - min + 1),
-      d_end(RAND_MAX - RAND_MAX % d_range), // multiple of d_range
-      d_buffer(0)
+: 
+    d_min(min),
+    d_range(max - min + 1),
+    d_end(RAND_MAX - RAND_MAX % d_range), // multiple of d_range
+    d_buffer(0)
 {
-  srandom(seed);
-  setg(0, 0, 0); // no buffer initially
+    srandom(seed);
+    setg(0, 0, 0); // no buffer initially
 }
