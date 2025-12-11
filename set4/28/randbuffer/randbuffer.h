21ba4df05a692c2943bcac8f65410d3111cdcd0d
boomhex <98780985+boomhex@users.noreply.github.com>
2025-12-11 22:59:19 +0100

    indent

--- set4/28/randbuffer/randbuffer.h
+++ set4/28/randbuffer/randbuffer.h
@@ -1,16 +1,16 @@
-  #ifndef INCLUDED_RANDBUFFER_
-  #define INCLUDED_RANDBUFFER_
+#ifndef INCLUDED_RANDBUFFER_
+#define INCLUDED_RANDBUFFER_
 
-  #include <streambuf>
+#include <streambuf>
 
-  class RandBuffer : public std::streambuf
-  {
-        int d_min;
-        size_t d_range;
-        size_t d_end;
-        char *d_buffer;
+class RandBuffer : public std::streambuf
+{
+    int d_min;
+    size_t d_range;
+    size_t d_end;
+    char *d_buffer;
 
-        using traits_type = std::streambuf::traits_type;
+    using traits_type = std::streambuf::traits_type;
 
     public:
         using int_type = traits_type::int_type;
@@ -20,6 +20,6 @@
 
     protected:
         int_type underflow() override;
-  };
+};
 
-  #endif
+#endif
