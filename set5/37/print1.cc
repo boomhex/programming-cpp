ed88ad7ed3b0ad47e7261fb33fa4ac2e0a6e91f6
RubenKorbeeck <rubenkorbeeck@gmail.com>
2026-01-06 11:37:56 +0100

    ex 37

---
+++ set5/37/print1.cc
@@ -0,0 +1,7 @@
+#include "main.ih"
+
+void printWords(vector<string> words)
+{
+    cout << "size=" << words.size()
+         << " capacity=" << words.capacity() << '\n';
+}
---
+++ set6/44/students/student.h
@@ -0,0 +1,15 @@
+#ifndef INCLUDED_STUDENT_
+#define INCLUDED_STUDENT_
+
+#include <string>
+
+class student
+{
+    std::string d_name;
+    std::string d_lastName;
+    size_t d_sNumber;
+    float d_grade;
+};
+
+
+#endif
