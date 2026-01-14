93235252057d678e1a9b1d88ffd09513c1a453c6
RubenKorbeeck <rubenkorbeeck@gmail.com>
2026-01-14 13:22:08 +0100

    new 42

--- set6/43/main.cc
+++ set6/43/main.cc
@@ -1,19 +1,4 @@
-#include "main.h"
-
-size_t multiples(vector<size_t> const &vs)
-{
-    size_t result = 0;
-
-    for (auto it = vs.begin(); it != vs.end(); )
-    {
-        //count how many times the current element appears (excluding the first) and add that to the result
-        result += count(it, vs.end(), *it) - 1;
-        //move the iterator to the next different element
-        it = upper_bound(it, vs.end(), *it);
-    }
-
-    return result;
-}
+#include "main.ih"
 
 int main()
 {
