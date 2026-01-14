e7807533ddf109faad592d9bd0c1bcd4c0e496f8
Julian <julianmounir@gmail.com>
2026-01-14 14:16:51 +0100

    fixed 47

--- set6/47/main.cc
+++ set6/47/main.cc
@@ -1,34 +1,13 @@
 #include "main.ih"
 
-// A struct that is used to customize the increment step 
-// used a struct because i did not see the need for any 
-// of the extra features of a class
-struct customIncrement
-{
-    unsigned value;
-    unsigned step;
-
-    customIncrement &operator++()
-    {
-        value += step;
-        return *this;
-    }
-
-    operator unsigned() const
-    {
-        return value;
-    }
-};
-
 int main()
 {
     vector<unsigned> vs(10);
 
-    //filling the vector with values starting from 0 and incrementing by 2
-    iota(vs.begin(), vs.end(), customIncrement{0,2});
-    
-    for (auto v : vs)
-        cout << v << ' ';
+    iota(vs.begin(), vs.end(), customIncrement(0, 2));
+
+    for (auto val : vs)
+        cout << val << ' ';
 
     cout << '\n';
 }
