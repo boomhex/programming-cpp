b7ddd0626e772f37200c2df23348b3ed8714109e
RubenKorbeeck <rubenkorbeeck@gmail.com>
2026-01-14 14:44:37 +0100

    44

--- set6/44/names.cc
+++ set6/44/names.cc
@@ -1,7 +1,14 @@
 #include "main.ih"
 
-void writeNames(ostream &out, vector<Student> const &vec)
+void writeNames(ostream &out, vector<Student> &vec)
 {
+    sort(vec.begin(), vec.end(),
+         [](Student const &stu1, Student const &stu2)
+         {
+             return strcasecmp(stu1.lastName().c_str(),
+                               stu2.lastName().c_str()) < 0;
+         });
+
     for (Student const &student : vec)
         out << student.name() 
             << ' ' << student.sNumber() 
--- set6/44/number.cc
+++ set6/44/number.cc
@@ -1,9 +1,17 @@
 #include "main.ih"
 
 void writeNrs(ostream &out, 
-              vector<Student> const &vec, 
-              vector<size_t> const &idx)
+              vector<Student> const &vec)
 {
+    vector<size_t> idx(vec.size());
+    iota(idx.begin(), idx.end(), 0);
+
+    sort(idx.begin(), idx.end(),
+         [&](size_t idx, size_t jdx)
+         {
+             return vec[idx].sNumber() < vec[jdx].sNumber();
+         });
+
     for (size_t num : idx)
         out << vec[num].sNumber() << ' ' << vec[num].grade() << '\n';
 }
