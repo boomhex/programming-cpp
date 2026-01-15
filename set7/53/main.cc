e099426535bf05ba55ad452607b69c7427de6ffc
boomhex <98780985+boomhex@users.noreply.github.com>
2026-01-15 16:57:47 +0100

    finished 53

--- set7/53/main.cc
+++ set7/53/main.cc
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv)
 {
-    MultiOut multi{ "8" };  // fixed for the exercise, use, e.g., argv[1]
+    MultiOut multi{ "3" };  // fixed for the exercise, use, e.g., argv[1]
                             // to specify the #threads as program argument
     multi.run();
 }
