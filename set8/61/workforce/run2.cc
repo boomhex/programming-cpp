384e368489af6961a2f14887f48111850b80938e
RubenKorbeeck <rubenkorbeeck@gmail.com>
2026-01-21 20:40:08 +0100

    61

--- set8/61/workforce/run2.cc
+++ set8/61/workforce/run2.cc
@@ -1,8 +1,35 @@
 #include "workforce.ih"
 
-#include "workforce.ih"
-
 void WorkForce::run(istream &input, ostream &output)
 {
+    vector<thread> admin;
+    admin.reserve(d_admin);
+    for (size_t idx = 0; idx != d_admin; ++idx)
+        admin.emplace_back(&WorkForce::administation, this, ref(output));
 
+    vector<thread> workers;
+    workers.reserve(d_workers);
+    for (size_t idx = 0; idx != d_workers; ++idx)
+        workers.emplace_back(&WorkForce::worker, this);
+
+    for (string line; getline(input, line); )
+    {
+        lock_guard<mutex> lg(d_workMutex);
+        d_workQueue.push(line);
+        rawLines.notify_all();
+    }
+
+    d_producerDone.store(true, memory_order_release);
+
+    for (size_t idx = 0; idx != d_workers; ++idx)
+        rawLines.notify();
+
+    for (auto &th : workers)
+        th.join();
+    
+    for (size_t idx = 0; idx != d_admin; ++idx)
+        reverseLines.notify();
+
+    for (auto &th : admin)
+        th.join();
 }
--- set8/61/workforce/workforce.h
+++ set8/61/workforce/workforce.h
@@ -13,19 +13,26 @@ class WorkForce
     size_t d_workers;
     size_t d_admin;
 
-    std::mutex workMutex;
+    std::mutex d_workMutex;
+    std::mutex d_paperworkMutex;
 
     std::queue<std::string> d_workQueue;
     std::queue<std::string> d_paperworkQueue;
 
-    Semaphore workers{d_workers};
-    Semaphore admin{d_admin};
+    Semaphore rawLines{0};
+    Semaphore reverseLines{0};
 
+    std::atomic_bool d_producerDone{false};
+    std::atomic_size_t d_workerDone{0};
 
     public:
         WorkForce();
         void run(std::string const &input, std::string const &output); // run1
         void run(std::istream &input, std::ostream &output);    //run2
+    
+    private:
+        void administation(std::ostream &output);
+        void worker();
 };
 
 #endif
