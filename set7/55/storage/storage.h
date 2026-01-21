ea84404887d5ef20b3e53e265cc3a0103b2dd8d0
boomhex <98780985+boomhex@users.noreply.github.com>
2026-01-21 21:56:23 +0100

    rm storage from 55

---
+++ set7/55/storage/storage.h
@@ -0,0 +1,25 @@
+#ifndef INCLUDED_STORAGE
+#define INCLUDED_STORAGE
+
+#include <string>
+#include <queue>
+#include <mutex>
+
+class Storage
+{
+    std::queue<std::string> d_queue;
+    mutable std::mutex d_qmutex;
+    bool d_finished;
+
+    public:
+        Storage();
+
+        bool pop(std::string &str);     // changed signature, see descr1.txt
+        void push(std::string string);
+        bool empty()    const;
+        void finished();
+};
+
+// see 54 for implementation
+
+#endif
